144bf37dfaab5f2594649d8907fdaa2c9de1cda8
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2023-11-27 04:42:04 +0000

    first

---
+++ Reame.md
@@ -0,0 +1,21 @@
+# Guess the Number Game
+
+## Description
+Guess the Number is a simple web-based game where the player tries to guess a randomly generated number between 1 and 10. The game provides feedback on each guess, indicating whether the guess is correct or incorrect. The player is informed about the number of attempts made, and the game ends when the correct number is guessed.
+
+## How to Play
+1. Open `index.html` in a web browser.
+2. Enter a number between 1 and 10 in the input field.
+3. Click the "Submit Guess" button to submit your guess.
+4. Receive feedback on whether your guess is correct or incorrect.
+5. Continue guessing until you correctly guess the random number.
+6. Once the correct number is guessed, the input field and button are disabled.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Author
+- Mouhamed Diouf
+- seydiahmedelcheikh@gmail.com
---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const secretNumber = Math.floor(Math.random() * 10) + 1;
+    let attempts = 0;
+
+    const guessInput = document.getElementById("guessInput");
+    const submitBtn = document.getElementById("submitBtn");
+    const message = document.getElementById("message");
+
+    submitBtn.addEventListener("click", function () {
+        const userGuess = parseInt(guessInput.value);
+
+        if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
+            setMessage("Please enter a valid number between 1 and 10.");
+            return;
+        }
+
+        attempts++;
+
+        if (userGuess === secretNumber) {
+            setMessage(`Congratulations! You guessed the number ${secretNumber} in ${attempts} attempts.`, "green");
+            disableInput();
+        } else {
+            setMessage(`Wrong guess. Try again. Attempts: ${attempts}`, "red");
+        }
+    });
+
+    function setMessage(msg, color = "black") {
+        message.textContent = msg;
+        message.style.color = color;
+    }
+
+    function disableInput() {
+        guessInput.disabled = true;
+        submitBtn.disabled = true;
+    }
+});
